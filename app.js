9f42931408622ee8eb3b9ec4087c1c9bb6e77cba
parth <parthsanjeevthakur9899@gmail.com>
2026-09-04 21:23:51 +0530

    Updated error handling

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing");
 const path=require("path");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const ExpressError=require("./utils/ExpressError");
+const wrapAsync=require("./utils/wrapAsync");
+const{listingSchema}=require("./schema.js");    
 
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"public"))); 
@@ -17,7 +20,13 @@ app.engine("ejs", ejsMate);
 app.get("/",(req,res)=>{
     res.send("working");
 })
-
+const ValidateSchema=(req,res,next)=>{
+    const {error}=listingSchema.validate(req.body);
+    if(error){
+        throw new ExpressError(400,error.details.map(el=>el.message).join(","));
+    }
+    next();
+}
 
 const Mongo='mongodb://127.0.0.1:27017/wanderlust';
 
@@ -45,49 +54,63 @@ async function main(){
 //     res.send("Sample listing created!");
 // });
 
-app.get("/listings" , async(req,res)=>{
+app.get("/listings" , wrapAsync(async(req,res)=>{
 
     let alllisting=await Listing.find({})
     res.render("index.ejs",{alllisting});
     
-});
+}));
 
 app.get("/listings/new",(req,res)=>{
     res.render("new.ejs");
 });
 
-app.post("/listings",async(req,res)=>{
+app.post("/listings",wrapAsync(async(req,res)=>{
+    listingSchema.validate(req.body);
+    if(listingSchema.validate(req.body).error){
+        const msg=listingSchema.validate(req.body).error.details.map(el=>el.message).join(",");
+        throw new ExpressError(400,msg);
+    }
     const {title,description,image,price,location,country}=req.body;
     const newListing=new Listing(req.body);
     await newListing.save();
     res.redirect("/listings");
-});
-
-app.get("/listings/:id",async(req,res)=>{
+}));
+ 
+app.get("/listings/:id",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     let listing=await Listing.findById(id);
     res.render("show.ejs",{listing});
-});
+}));
 
-app.get("/listings/:id/edit",async(req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     let listing=await Listing.findById(id);
     res.render("edit.ejs",{listing});
-});
+}));
 
-app.put("/listings/:id",async(req,res)=>{
+app.put("/listings/:id",ValidateSchema ,wrapAsync(async(req,res)=>{
     const {id}=req.params;
     let listing=await Listing.findByIdAndUpdate(id,req.body);
     res.redirect(`/listings/${listing._id}`);
-});
-app.delete("/listings/:id",async(req,res)=>{
+}));
+app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     const {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings");
+}));
+app.all("/*splat",(req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found"));
 });
 
 app.listen(8080,()=>{
     console.log("server is running at port 8080");
 
+});
+app.use((err,req,res,next)=>{  
+    
+    let {statusCode=500,message="Something went wrong"}=err;     
+    // res.status(statusCode).send(message);
+    res.render("error.ejs",{err})
 });
 
